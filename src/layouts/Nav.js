c327c63c66ea9b46410dad45157e13abae92fbc4
polawebsite <pola.web.development@gmail.com>
2025-03-03 23:13:10 +0700

    initial commit

--- src/layouts/Nav.js
+++ src/layouts/Nav.js
@@ -10,8 +10,8 @@ function Nav() {
       {/* Logo and Text */}
       <Link to="/Admin/Articles" className="flex items-center space-x-4">
         <img
-          src="https://res.cloudinary.com/dwcbcgccc/image/upload/v1740202928/image/wsb1ru5kha87dq1tkfif.png"
-          className="w-32 h-14 rounded" // Adjusted size
+          src="https://res.cloudinary.com/dwcbcgccc/image/upload/v1740653689/image/j8dgk9qjysck7zsbhkih.png"
+          className="w-30 h-14 rounded" // Adjusted size
           alt="Logo"
         />
         <span className="font-bold text-l">Blog Admin</span>
@@ -29,7 +29,7 @@ function Nav() {
           </p>
           <div className="w-10 h-10 bg-[#F8F8F8] flex items-center justify-center rounded-sm">
             <img
-              src="https://res.cloudinary.com/dwcbcgccc/image/upload/v1740202928/image/ybn9opbrw7bfq3ooxtcm.png"
+              src="https://res.cloudinary.com/dwcbcgccc/image/upload/v1740653688/image/qi3es2vzokah3rfeihme.png"
               className="w-8 h-8 object-cover" // Ensures the image stays in bounds
               alt="Profile"
             />
